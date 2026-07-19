4b168cb669d0a6a2c914b8e77eb8833350f7639d
shwetha227 <mshwethashwetha79@gmail.com>
2026-07-19 12:59:25 +0000

    Extract individual video/audio streams into JSON streams array

--- c/inspector.c
+++ c/inspector.c
@@ -44,6 +44,75 @@ static void sb_appendf(strbuf *sb, const char *fmt, ...) {
     sb_append(sb, tmp);
 }
 
+static void append_stream_json(strbuf *sb, GstDiscovererStreamInfo *stream, gboolean *first) {
+    GstCaps *caps = gst_discoverer_stream_info_get_caps(stream);
+    if (caps == NULL) {
+        return;
+    }
+
+    const GstStructure *s = gst_caps_get_structure(caps, 0);
+    const gchar *media_type = gst_structure_get_name(s);
+
+    if (!*first) {
+        sb_append(sb, ",");
+    }
+    *first = FALSE;
+
+    sb_append(sb, "{");
+    sb_append(sb, "\"codec\":\"");
+    sb_append(sb, media_type);
+    sb_append(sb, "\"");
+
+    if (GST_IS_DISCOVERER_VIDEO_INFO(stream)) {
+        GstDiscovererVideoInfo *vinfo = GST_DISCOVERER_VIDEO_INFO(stream);
+        guint width = gst_discoverer_video_info_get_width(vinfo);
+        guint height = gst_discoverer_video_info_get_height(vinfo);
+        guint fps_n = gst_discoverer_video_info_get_framerate_num(vinfo);
+        guint fps_d = gst_discoverer_video_info_get_framerate_denom(vinfo);
+
+        sb_append(sb, ",\"type\":\"video\"");
+        sb_appendf(sb, ",\"width\":%u,\"height\":%u", width, height);
+        if (fps_d > 0) {
+            sb_appendf(sb, ",\"fps\":\"%u/%u\"", fps_n, fps_d);
+        }
+    } else if (GST_IS_DISCOVERER_AUDIO_INFO(stream)) {
+        GstDiscovererAudioInfo *ainfo = GST_DISCOVERER_AUDIO_INFO(stream);
+        guint channels = gst_discoverer_audio_info_get_channels(ainfo);
+        guint sample_rate = gst_discoverer_audio_info_get_sample_rate(ainfo);
+
+        sb_append(sb, ",\"type\":\"audio\"");
+        sb_appendf(sb, ",\"channels\":%u,\"sample_rate\":%u", channels, sample_rate);
+    } else {
+        sb_append(sb, ",\"type\":\"unknown\"");
+    }
+
+    sb_append(sb, "}");
+    gst_caps_unref(caps);
+}
+
+static void collect_streams(strbuf *sb, GstDiscovererStreamInfo *info, gboolean *first) {
+    if (info == NULL) {
+        return;
+    }
+
+    if (GST_IS_DISCOVERER_CONTAINER_INFO(info)) {
+        GstDiscovererContainerInfo *cinfo = GST_DISCOVERER_CONTAINER_INFO(info);
+        GList *children = gst_discoverer_container_info_get_streams(cinfo);
+        for (GList *l = children; l != NULL; l = l->next) {
+            collect_streams(sb, GST_DISCOVERER_STREAM_INFO(l->data), first);
+        }
+        gst_discoverer_stream_info_list_free(children);
+    } else {
+        append_stream_json(sb, info, first);
+    }
+
+    GstDiscovererStreamInfo *next = gst_discoverer_stream_info_get_next(info);
+    if (next != NULL) {
+        collect_streams(sb, next, first);
+        gst_discoverer_stream_info_unref(next);
+    }
+}
+
 char *inspect_media(const char *filepath) {
     if (filepath == NULL || filepath[0] == '\0') {
         return NULL;
@@ -107,6 +176,11 @@ char *inspect_media(const char *filepath) {
         sb_append(&sb, "unknown");
     }
     sb_append(&sb, "\"");
+
+    sb_append(&sb, ",\"streams\":[");
+    gboolean first_stream = TRUE;
+    collect_streams(&sb, stream_info, &first_stream);
+    sb_append(&sb, "]");
 
     if (stream_info != NULL) {
         gst_discoverer_stream_info_unref(stream_info);
